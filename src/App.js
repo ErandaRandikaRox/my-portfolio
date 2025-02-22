5a1e93030bdb292de5c5f425eb0c36cd46616958
ErandaRandikaRox <160154662+ErandaRandikaRox@users.noreply.github.com>
2025-02-22 15:42:07 +0530

    create pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Navbar from './components/Navbar';
+import Intro from './components/Intro';
+import Services from './components/Services';
+import Experience from './components/Experience';
+import Contact from './components/Contact';
+import AboutMe from './components/AboutMe';
+import './styles.css'; // Import the new CSS file
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar />
+      <Intro />
+      <Services />
+      <Experience />
+      <Contact />
+      <AboutMe />
     </div>
   );
 }
